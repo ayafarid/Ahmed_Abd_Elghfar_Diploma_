5d7c240249cfe9cd614e4286861f977c5dea3f6e
Aya <ayafarid700@gmail.com>
2023-08-12 16:10:15 +0300

    commit second 14 problem in functions

--- C part/Task_2_functions/Code/main.c
+++ C part/Task_2_functions/Code/main.c
@@ -20,6 +20,19 @@ void display_perfect(int lowRange,int highRange);
 long long fact(int num);
 int is_strong(int num);
 int power_using_recursion(int num,int power);
+void display_range(int lowerLimit,int upperLimit);
+void display_range_odd(int lowerLimit,int upperLimit);
+void display_range_even(int lowerLimit,int upperLimit);
+int sum_range(int lowerLimit,int upperLimit);
+int reverse_number(int num);
+void check_palindrome(int num);
+int sum_of_digit(int num);
+int factorial(int num);
+int fibonacci(int num);
+int greatestCommonDivisor(int num1,int num2);
+int lowestCommonMultiple(int a, int b);
+void print_array_elements(int array[],int size);
+int sum_of_array_elements(int array[],int size);
 int main()
 {
     /// TASK ONE
@@ -109,7 +122,7 @@ int main()
     scanf("%d",&upperLimit);
     printf("Armstrong numbers between %d-%d are: ",lowerLimit,upperLimit);
     display_armstrong(lowerLimit,upperLimit);
-#endif // 0
+#endif
     /// TASK NINE
 #if 0
     int lowerLimit,upperLimit;
@@ -119,7 +132,7 @@ int main()
     scanf("%d",&upperLimit);
     printf("Armstrong numbers between %d-%d are: ",lowerLimit,upperLimit);
     display_perfect(lowerLimit,upperLimit);
-#endif // 0
+#endif
     /// TASK TEN
 #if 0
     int number,power;
@@ -128,7 +141,150 @@ int main()
     printf("Input power: ");
     scanf("%d",&power);
     printf("%d ^ %d = %d",number,power,power_using_recursion(number,power));
-#endif // 0
+#endif
+    /// TASK 11
+#if 0
+    int lowerLimit,upperLimit;
+    printf("Input lower limit: ");
+    scanf("%d",&lowerLimit);
+    printf("Input upper limit: ");
+    scanf("%d",&upperLimit);
+    printf("Natural numbers between %d to %d: ",lowerLimit,upperLimit);
+    display_range(lowerLimit,upperLimit);
+#endif
+    /// TASK 12
+#if 0
+    int lowerLimit,upperLimit;
+    printf("Input lower limit: ");
+    scanf("%d",&lowerLimit);
+    printf("Input upper limit: ");
+    scanf("%d",&upperLimit);
+    printf("Even numbers between %d to %d: ",lowerLimit,upperLimit);
+    display_range_even(lowerLimit,upperLimit);
+    printf("\n");
+    printf("Odd numbers between %d to %d: ",lowerLimit,upperLimit);
+    display_range_odd(lowerLimit,upperLimit);
+#endif
+    /// TASK 13
+#if 0
+    int lowerLimit,upperLimit;
+    printf("Input lower limit: ");
+    scanf("%d",&lowerLimit);
+    printf("Input upper limit: ");
+    scanf("%d",&upperLimit);
+    printf("Even numbers between %d to %d: ",lowerLimit,upperLimit);
+    display_range_even(lowerLimit,upperLimit);
+    printf("\n");
+    printf("Odd numbers between %d to %d: ",lowerLimit,upperLimit);
+    display_range_odd(lowerLimit,upperLimit);
+#endif
+    /// TASK 14
+#if 0
+    int lowerLimit,upperLimit;
+    printf("Input lower limit: ");
+    scanf("%d",&lowerLimit);
+    printf("Input upper limit: ");
+    scanf("%d",&upperLimit);
+    printf("Sum of natural numbers from %d to %d = %d",lowerLimit,upperLimit,sum_range(lowerLimit,upperLimit));
+
+#endif
+    /// TASK 15
+#if 0
+    int num;
+    printf("Input number: ");
+    scanf("%d",&num);
+    printf("Reverse: %d",reverse_number(num));
+#endif
+    /// TASK 16
+#if 0
+    int num;
+    printf("Input number: ");
+    scanf("%d",&num);
+    check_palindrome(num);
+#endif
+    /// TASK 17
+#if 0
+    int num;
+    printf("Input number: ");
+    scanf("%d",&num);
+    printf("Sum of digits: %d",sum_of_digit(num));
+#endif
+    /// TASK 18
+#if 0
+    int num;
+    printf("Input number: ");
+    scanf("%d",&num);
+    printf("Factorial of %d = %d",num,factorial(num));
+#endif
+    /// TASK 19
+#if 0
+    int num;
+    printf("Input number: ");
+    scanf("%d",&num);
+    printf("Fibonacci of %d = %d",num,fibonacci(num));
+#endif=
+    /// TASK 20
+#if 0
+    int num1,num2;
+    printf("Input first number: ");
+    scanf("%d",&num1);
+    printf("Input second number: ");
+    scanf("%d",&num2);
+    printf("HCF of %d and %d = %d",num1,num2,greatestCommonDivisor(num1,num2));
+#endif
+    /// TASK 21
+#if 0
+    int num1,num2;
+    printf("Input first number: ");
+    scanf("%d",&num1);
+    printf("Input second number: ");
+    scanf("%d",&num2);
+    int LCM;
+    if(num1>num2)
+        LCM=lowestCommonMultiple(num2,num1);
+    else
+        LCM=lowestCommonMultiple(num1,num2);
+    printf("LCM of %d and %d = %d",num1,num2,LCM);
+#endif
+    /// TASK 22
+#if 0
+    int size;
+    printf("Input size: ");
+    scanf("%d",&size);
+    int array[size];
+    printf("Input elements: ");
+    for(int i=0;i<size;i++){
+        scanf("%d",&array[i]);
+    }
+    printf("Array elements: ");
+    print_array_elements(array,size);
+#endif
+    /// TASK 23
+#if 0
+    int size;
+    printf("Input size: ");
+    scanf("%d",&size);
+    int array[size];
+    printf("Input elements: ");
+    for(int i=0;i<size;i++){
+        scanf("%d",&array[i]);
+    }
+    printf("Sum of array: %d",sum_of_array_elements(array,size));
+#endif
+    /// TASK 24
+#if 0
+    int size;
+    printf("Input size: ");
+    scanf("%d",&size);
+    int array[size];
+    printf("Input elements: ");
+    for(int i=0;i<size;i++){
+        scanf("%d",&array[i]);
+    }
+    int min=array[0],max=array[0];
+    max_and_min(array,size,&max,&min);
+    printf("Maximum element = %d\nMinimum element = %d",max,min);
+#endif
     return 0;
 }
 int cube_of_a_number(int number){
@@ -202,7 +358,7 @@ int is_armstong(int num){
     else
         return 0;
 #if 0
-        int lastDigit, sum, originalNum, digits;
+    int lastDigit, sum, originalNum, digits;
     sum = 0;
 
     originalNum = num;
@@ -267,8 +423,7 @@ void dispaly_strong(int lowRange,int highRange){
             printf("%d ",i);
     }
 }
-long long fact(int num)
-{
+long long fact(int num){
     if(num == 0)
         return 1;
     else
@@ -291,4 +446,137 @@ int power_using_recursion(int num,int power){
         return 1;
     else
         return num*power_using_recursion(num,power-1);
+}
+void display_range(int lowerLimit,int upperLimit){
+    if(lowerLimit>upperLimit)
+        return;
+    else{
+        printf("%d ,",lowerLimit);
+        lowerLimit++;
+        display_range(lowerLimit,upperLimit);
+    }
+}
+void display_range_odd(int lowerLimit,int upperLimit){
+    if(lowerLimit>upperLimit)
+        return;
+    else{
+        if(lowerLimit%2!=0){
+             printf("%d ,",lowerLimit);
+        }
+        lowerLimit++;
+        display_range_odd(lowerLimit,upperLimit);
+    }
+}
+void display_range_even(int lowerLimit,int upperLimit){
+    if(lowerLimit>upperLimit)
+        return;
+    else{
+        if(lowerLimit%2==0){
+            printf("%d ,",lowerLimit);
+        }
+        lowerLimit++;
+        display_range_even(lowerLimit,upperLimit);
+    }
+}
+int sum_range(int lowerLimit,int upperLimit){
+    if(lowerLimit>upperLimit)
+        return 0;
+    else{
+        return lowerLimit+sum_range(lowerLimit+1,upperLimit);
+    }
+}
+int reverse_number(int num){
+    int num_digit=(int) log10(num);
+    int product=pow(10,num_digit);
+    int reversed_num=0;
+    while(product){
+        reversed_num+=product*(num%10);
+        num/=10;
+        product/=10;
+    }
+    return reversed_num;
+}
+void check_palindrome(int num){
+    if(reverse_number(num)==num)
+        printf("%d is palindrome",num);
+    else
+        printf("%d is not palindrome",num);
+}
+int sum_of_digit(int num){
+    int sum;
+    while(num){
+        sum+=num%10;
+        num/=10;
+    }
+    return sum;
+}
+int factorial(int num){
+    if(num==0)
+        return 1;
+    else
+        return num*factorial(num-1);
+}
+int fibonacci(int num){
+    if(num==0)
+        return 0;
+    else if(num==1)
+        return 1;
+    else if(num>1){
+        return fibonacci(num-1)+fibonacci(num-2);
+    }
+}
+int greatestCommonDivisor(int num1,int num2){
+    if(num2==0)
+        return num1;
+    else{
+        return greatestCommonDivisor(num2,num1%num2);
+    }
+
+}
+int lowestCommonMultiple(int a, int b){
+    static int multiple = 0;
+    multiple += b;
+    if((multiple % a == 0) && (multiple % b == 0))
+    {
+        return multiple;
+    }
+    else
+    {
+        return lowestCommonMultiple(a, b);
+    }
+}
+void print_array_elements(int array[],int size){
+    static int i=0;
+    if(i==size)
+        return;
+    else{
+        printf("%d ,",array[i]);
+        i++;
+        print_array_elements(array,size);
+    }
+}
+int sum_of_array_elements(int array[],int size){
+    static int i=0,sum=0;
+    if(i==size)
+        return sum;
+    else{
+        sum+=array[i];
+        i++;
+        sum_of_array_elements(array,size);
+        return sum;
+    }
+}
+void max_and_min(int array[],int size,int* max,int* min){
+    static int i=0;
+    if(i==size)
+        return;
+    else{
+        if(*max<array[i])
+            *max=array[i];
+        if(*min>array[i])
+            *min=array[i];
+        i++;
+        max_and_min(array,size,max,min);
+        return;
+    }
 }
